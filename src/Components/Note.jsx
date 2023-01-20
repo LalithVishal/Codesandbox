6d465ca06b37f4ea496bad88ab11f92a6d0ee3f7
LALITH VISHAL <arimpakamlalithvishal@gmail.com>
2023-01-20 15:26:45 +0000

    Update 7 files

---
+++ src/Components/Note.jsx
@@ -0,0 +1,14 @@
+var react = require("react");
+
+function Note() {
+    return (
+        <div classname ="Note">
+          <h1>DevTown</h1>
+          <p>This is code of react.js</p>
+          </div>
+          
+              
+    );
+}
+
+export default Note;
